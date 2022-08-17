21ac385d42a38c52fba4979cfa18f9305687e20b
caioffreire <caioffreire1@gmail.com>
2022-08-17 18:26:27 -0300

    Adicionando o fim do jogo.

--- Day 4/Day4_Mais_Loops_e_Randomizacao.js
+++ Day 4/Day4_Mais_Loops_e_Randomizacao.js
@@ -7,15 +7,24 @@ console.log(numeroSorteado);
 
 let tentativas = 1;
 
-while(tentativas <=3){
+while(tentativas <= 3){
+
     let chute = parseInt(prompt("Essa é a sua chance! Chute um número de 0 a 10!"));
 
     if(chute == numeroSorteado) {
+
         alert("Parabéns! Você acertou! Realmente pensei em " + numeroSorteado + "!");
         break;
-    } else {
+    
+    }else{
         alert("Você errou!")
     }
 
     tentativas++;
+   
+    }
+
+if (tentativas = 3 || chute != numeroSorteado){
+    
+        alert("Fim de jogo! O número correto era " + numeroSorteado + "!")
 }
