b325ef27c13c431d855f82b106ac4e82ad827847
caioffreire <caioffreire1@gmail.com>
2022-08-17 15:38:42 -0300

    Adicionando o dia 4.

---
+++ Day 4/Day4_Mais_Loops_e_Randomizacao.js
@@ -0,0 +1,21 @@
+function sorteio(n){
+    return Math.round(Math.random() * 10);
+}
+
+let numeroSorteado = sorteio();
+console.log(numeroSorteado);
+
+let tentativas = 1;
+
+while(tentativas <=3){
+    let chute = parseInt(prompt("Essa é a sua chance! Chute um número de 0 a 10!"));
+
+    if(chute == numeroSorteado) {
+        alert("Parabéns! Você acertou! Realmente pensei em " + numeroSorteado + "!");
+        break;
+    } else {
+        alert("Você errou!")
+    }
+
+    tentativas++;
+}
